b6f99553e285a40d40f61ec8eb5aced4f897a3bb
Brandon Liu <bliu88@live.com>
2019-02-05 21:22:54 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,5 @@
 # code your #valid_move? method here
-
+def valid_move?
+  if position_taken(number) == 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
